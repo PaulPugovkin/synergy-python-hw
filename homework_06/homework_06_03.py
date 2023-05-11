2e27eed9787021999516bcb7ca80895699eb0281
PaulPugovkin <coastalrhodes@gmail.com>
2023-05-11 14:45:24 +0300

    hw 06(tasks 1,2,3)

---
+++ homework_06/homework_06_03.py
@@ -0,0 +1,12 @@
+# Вводятся целые числа A и B. Гарантируется, что A ≤ B.
+# Выведите все четные числа на заданном отрезке через пробел.
+
+numA = int(input("Введите число A: "))
+numB = int(input("Введите число B: "))
+oddNumbers = 0
+
+for i in range(numA, numB+1):
+    print(i)
+    if (i % 2 == 0) and not (i == 0):
+        oddNumbers += 1
+print("Количество четных чисел: ", oddNumbers)
